86e0f838d6bdf5a517afc0737a3bedc1e63a9dbb
undeadtokenart <undeadtokenart@gmail.com>
2023-03-04 15:32:51 -0800

    Added some functions that give data on a text file

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-with open(books/frankenstein.txt):
-    file_contents = f.read()
+def get_num_of_char(path):
+    with open(path) as f:
+        text = f.read().lower()
+        char_count = {}
+        for i in text:
+            if i in char_count:
+                char_count[i] += 1
+            else:
+                char_count[i] = 1
+    return char_count
 
-    def num_words():
-        words = s.split()
-        print(words)
+
+
+# gets the number of words from a given path
+def get_word_count(path):
+    with open(path) as f:
+        text = f.read()
+        wordcount = len(text.split())
+        return (str(wordcount))
     
-    numwords()
+
+letter_data = get_num_of_char("books/frankenstein.txt")
+word_data = get_word_count("books/frankenstein.txt")
+for i in 
