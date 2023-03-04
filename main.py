3b52937ee8f7e45ed2dd62226105957db097e634
undeadtokenart <undeadtokenart@gmail.com>
2023-03-04 15:56:47 -0800

    Added a reporting printout on the text

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def get_num_of_char(path):
         text = f.read().lower()
         char_count = {}
         for i in text:
-            if i in char_count:
+            if i.isalpha() == False:
+                None
+            elif i in char_count:
                 char_count[i] += 1
             else:
                 char_count[i] = 1
@@ -21,4 +23,12 @@ def get_word_count(path):
 
 letter_data = get_num_of_char("books/frankenstein.txt")
 word_data = get_word_count("books/frankenstein.txt")
-for i in 
+
+
+print("---------- Start of report ----------")
+print(word_data)
+
+for i in letter_data:
+    print(f"The '{i}' character was found {letter_data[i]} times.")
+
+print("--------- End of report ----------")
